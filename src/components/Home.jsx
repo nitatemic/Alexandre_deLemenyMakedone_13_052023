9851a1f878425c29411f997d4393fd0e95b3708a
nitatemic <alexandre@nitatemic.dev>
2023-05-01 16:41:43 +0200

    Features section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './Hero';
+import Features from './Features';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+        <Features />
     </main>
   );
 }
