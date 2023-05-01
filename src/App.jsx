4e8005e0224e22631fa7a971ec69421ca1f0b108
nitatemic <alexandre@nitatemic.dev>
2023-05-01 16:36:32 +0200

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './scss/_main.scss';
-import {BrowserRouter as Router, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
       <Router>
         <div className="headerAndMain">
-          <Navbar/>
+          <Navbar />
           <main className="main">
             <Routes>
-              {/* <Route exact path="/" element={<Home />} /> */}
+              <Route exact path="/" element={<Home />} />
               {/* Route 404 */}
               {/* <Route path="*" element={<Error />} /> */}
             </Routes>
