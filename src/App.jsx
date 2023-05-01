4a3e1b2e1f2512fb72a53bd1225f47bae796f832
nitatemic <alexandre@nitatemic.dev>
2023-05-01 15:55:26 +0200

    Init + Navbar

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './scss/_main.scss';
+import {BrowserRouter as Router, Routes} from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div className="headerAndMain">
+          <Navbar/>
+          <main className="main">
+            <Routes>
+              {/* <Route exact path="/" element={<Home />} /> */}
+              {/* Route 404 */}
+              {/* <Route path="*" element={<Error />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
